648291da04f5661125cc1698f86dba28d89f9d06
trirashivedi <72864595+trirashivedi@users.noreply.github.com>
2024-02-18 11:50:20 +0000

    testadd

--- 01-basics/test.js
+++ 01-basics/test.js
@@ -1,1 +1,37 @@
-console.log("Rashi");
+// console.log("Rashi");
+
+
+//primitive
+
+//7types : string,Number, Boolean, Null, Undefined,s
+  Symbol,BigInt
+
+//    const score = 100
+//    const scorevalue = 100.3
+
+//    const isLoggedIn = false 
+//    const outsideTemp = null
+//    let userEmail;
+
+   const id =  Symbol('123')
+   const anotherId = Symbol('123');
+
+    console.log(id === anotherId);
+
+  //Reference (Non primitive)
+
+  //Array, Objects, Functions
+
+  const heros = ["shatiman","naagraj","doga"];
+
+  let myObj= {
+    name:"Rashu",
+    age:23,
+  }
+
+
+  const myFunction = function(){
+     console.log("Hello world");
+  }
+  
+//   console.log("")
