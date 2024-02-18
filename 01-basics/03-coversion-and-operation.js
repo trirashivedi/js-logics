99e35b04c4454f270643f6da04f5ee2aa64eb51a
trirashivedi <72864595+trirashivedi@users.noreply.github.com>
2024-02-18 05:34:33 +0000

    coversion

---
+++ 01-basics/03-coversion-and-operation.js
@@ -0,0 +1,25 @@
+// /let score = undefined;
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber);
+
+//"33" ->33
+//"33abc" ->NaN
+//
+
+let isLoggedIn = ""
+
+let booleanIsLogggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLogggedIn);
+
+
+let someNumber = 33
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
