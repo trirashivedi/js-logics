ca73a1d3c3a42e27f6879907835e1023cd38857e
trirashivedi <72864595+trirashivedi@users.noreply.github.com>
2024-02-18 04:49:42 +0000

    datatypes

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,15 @@
+const accountId = 144553;
+let accountEmail = "trivedi.rashi95@gmail.com";
+var accountPassword = "1234560";
+accountCity="jaipur";
+let accountState;
+
+// accountId = 2;
+
+accountEmail = "dssdf@gmail.com"
+accountPassword = "15263636"
+accountCity = "ujjain"
+
+console.log(accountEmail);
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,6 @@
+"use-strict"; //treat allJs code as newer version
+
+// alert("") we are useing nodejs , not browser
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); //object
