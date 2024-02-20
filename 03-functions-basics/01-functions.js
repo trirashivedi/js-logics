13a1a1ce9d43a237360876d01edf51f723666785
trirashivedi <72864595+trirashivedi@users.noreply.github.com>
2024-02-20 08:31:04 +0000

    functions

---
+++ 03-functions-basics/01-functions.js
@@ -0,0 +1,24 @@
+function sayMyname(){
+    console.log("R");
+    console.log("a");
+    console.log("s");
+    console.log("h");
+    console.log("i");
+}
+//sayMyname (refrence)
+
+//sayMyname()
+
+function addTwonumbers(number1,number2){
+    // console.log(number1+number2);
+    return number1 + number2
+}
+
+// addTwonumbers(3,4)
+// addTwonumbers(3,null)
+
+
+
+const result = addTwonumbers(3,4)
+console.log("Result:",result);
+
